e08d73181aef5cdbd1a8c262dbdfc1fce9754e7b
superkite1 <1142635504@qq.com>
2018-09-26 23:08:54 +0800

    mathtable

---
+++ MathTable.java
@@ -0,0 +1,18 @@
+package math02;
+
+public class MathTable {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				if (j <= i) {
+					System.out.print(j + "*" + i + "=" + (j * i) + " ");
+				}
+			}
+			System.out.println();// 小括号里面的代码每执行一次换行
+		}
+	}
+
+}
